ee6afd66e879653f89d7b75d7ca619666730d3bd
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-20 07:04:08 +0000

    New

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
   def index
     @events = Event.where(start: params[:start]..params[:end])
   end
