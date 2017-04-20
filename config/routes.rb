f23062eab8ae935cbce08101e8accf7e0b8f7d93
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-20 05:15:49 +0000

    New

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  # get 'visitors/index'
+
+  # get 'events/index'
+
+  # get 'events/new'
+
+  # get 'events/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  resources :events
+  root 'visitors#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
