05d618bea48917d8b88a838187e4bf6aea84b549
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-20 05:26:27 +0000

    New

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,22 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.where(start: params[:start]..params[:end])
+  end
+  
+  def show
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
   end
+  
+  def create
+    @event = Event.new(event_params)
+    @event.save
+  end
+  
+  
 end
